1b76d63ff65804471e9a081ff2ba10e4b612616a
Arun Kumar Rajput <63948008+Arunrajput911@users.noreply.github.com>
2020-06-20 10:48:07 +0530

    data upadte

--- script.js
+++ script.js
@@ -11,3 +11,52 @@ $("#click-btn").click(function(){
     $(".container-third-hide").css("display","block");
     $(".container-third-content").toggle();
 });
+
+
+init()
+
+function init(){
+        var url1="https://api.covid19api.com/summary"
+        var active='';var recover='';var death='';
+        $.get(url1,function(data1){
+active=`<td>${data1.Countries[76]["TotalConfirmed"]}</td>`
+recover=`<td>${data1.Countries[76]["TotalRecovered"]}</td>`
+death=`<td>${data1.Countries[76]["TotalDeaths"]}</td>` 
+
+$("#active").html(active);
+$("#recover").html(recover);
+$("#death").html(death);
+        })
+
+// statewise update
+var url2="https://api.covid19india.org/data.json"
+$.get(url2,function(data2){
+    array=[12,32,16,13,21,25,4,9,26,22,11,19,8,1,27,36,31,30,17,18,6,33,2,14,23,5,20,7];
+    var value='';
+    console.log(data2.statewise[array[25]]);
+    for(var i=0;i<28;i++){
+        value=`<td>${data2.statewise[array[i]]["active"]}</td>
+        <td>${data2.statewise[array[i]]["recovered"]}</td>
+        <td>${data2.statewise[array[i]]["deaths"]}</td>`
+
+        $("#state"+i).html(value);
+    }
+   
+})
+
+}
+
+function Refresh(){
+    clearData();
+}
+function clearData(){
+    $("#active").empty();
+    $("#recover").empty();
+    $("#death").empty();
+    for(var i=0;i<27;i++){
+        $("#state"+i).empty();
+    }
+    
+    init();
+}
+
